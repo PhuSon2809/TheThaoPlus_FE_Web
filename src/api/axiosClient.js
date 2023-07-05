86be3745dae8316149d342feec140a5d984a851d
Tran Phu Son <phuson2809@gmail.com>
2023-07-05 11:07:49 +0700

    Config api ủl

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = process.env.REACT_APP_API;
+axios.defaults.baseURL = 'https://sport-plus-backend.azurewebsites.net/api';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 axios.defaults.headers.delete['Access-Control-Allow-Origin'] = '*';
